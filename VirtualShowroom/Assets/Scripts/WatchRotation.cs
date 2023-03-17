4650a64c12835bedc3c22d9070950122eb0d743e
Mei Jiang <jiangme@cronos.be>
2023-03-17 13:50:46 +0100

    Adjusted script WatchButton
    Fixed button problem when exit trigger zone

--- VirtualShowroom/Assets/Scripts/WatchRotation.cs
+++ VirtualShowroom/Assets/Scripts/WatchRotation.cs
@@ -28,9 +28,9 @@ public class WatchRotation : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(watchRotation * speed * Time.deltaTime);
+        //transform.Rotate(watchRotation * speed * Time.deltaTime);
 
-        if (/*tac.isInTriggerZone && */tac.triggerZone == "TriggerWatch")
+        if (tac.triggerZone == "TriggerWatch")
         {
             //Debug.Log("isopenanimpalyed: " + watchButton.isOpenAnimPlayed);
             if (watchButton.isOpenAnimPlayed == false)
@@ -43,7 +43,7 @@ public class WatchRotation : MonoBehaviour
                 openWatchButton.gameObject.SetActive(false);
                 closeWatchButton.gameObject.SetActive(true);
             }
-            
+            openWatchButton.gameObject.SetActive(true);
             if (Input.GetMouseButton(0))
             {
                 transform.eulerAngles += speed * new Vector3(x: 0/*-Input.GetAxis("Mouse Y")*/, y: Input.GetAxis("Mouse X"), z: 0);
@@ -52,19 +52,19 @@ public class WatchRotation : MonoBehaviour
         }
         else
         {
-            openWatchButton.gameObject.SetActive(false);
-            closeWatchButton.gameObject.SetActive(false);
+            if (watchButton.isOpenAnimPlayed)
+            {
+                watchButton.CloseWatch();
+            }
             if (currentRotation != standardRotation)
             {
                 transform.eulerAngles = standardRotation; //werkt maar snapt naar die positie
                 //transform.Rotate(standardRotation); //werkt niet: voert t wel uit maar  ziet het niet
                 currentRotation = standardRotation;
-                watchButton.isOpenAnimPlayed = false;
-
-                //hier code voor sluit anim
-                watchButton.CloseWatch();
+                //watchButton.isOpenAnimPlayed = false;
             }
-            
+            openWatchButton.gameObject.SetActive(false);
+            closeWatchButton.gameObject.SetActive(false);
         }
     }
 }
