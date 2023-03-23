e4987b9e033323c87413f7e3f2294cde7c2c8d5f
Mei Jiang <jiangme@cronos.be>
2023-03-23 14:39:40 +0100

    Added car rotation, added configurator buttons, deleted color picker
    Added script for rotating the car, added ui elements for configurator buttons, added script configurator button, deleted color picker asset

--- VirtualShowroom/Assets/Scripts/CarConfigurator.cs
+++ VirtualShowroom/Assets/Scripts/CarConfigurator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarConfigurator : MonoBehaviour
 {
@@ -8,17 +9,69 @@ public class CarConfigurator : MonoBehaviour
      *check if it s looking from the outside or inside
      */
     private TriggerActionsCamera tac;
+    private GameObject carUI;
+
+    private Button openCarConfButton;
+    private Button closeCarConfButton;
+    private CarConfButton ccb;
+    private GameObject carConfUI;
+
+    //private ApplyColor applyColor;
+
 
     void Start()
     {
         tac = GameObject.Find("Main Camera").GetComponent<TriggerActionsCamera>();
-    }
+        carUI = GameObject.Find("UICar");
 
+        openCarConfButton = GameObject.Find("OpenCarConf").GetComponent<Button>();
+        closeCarConfButton = GameObject.Find("CloseCarConf").GetComponent<Button>();
+        ccb = gameObject.GetComponent<CarConfButton>();
+        carConfUI = GameObject.Find("CarConf");
+
+    }
+    //error: applyColor.carMaterial bestaat niet
     void Update()
     {
         if (tac.triggerZone == "TriggerCar")
         {
-            Debug.Log("zone car");
+            //Debug.Log(gameObject.GetComponent<CarConfButton>());
+            carUI.SetActive(true);
+
+            if (ccb.isConfOpen == false)
+            {
+                openCarConfButton.gameObject.SetActive(true);
+                closeCarConfButton.gameObject.SetActive(false);
+
+                carConfUI.SetActive(false);
+            }
+            else
+            {
+                openCarConfButton.gameObject.SetActive(false);
+                closeCarConfButton.gameObject.SetActive(true);
+
+                carConfUI.SetActive(true);
+            }
         }
+        else
+        {
+            //Debug.Log(applyColor.carMaterial);
+            ResetCar();
+            ResetCarUI();
+        }
+    }
+
+    private void ResetCar()
+    {
+        //Debug.Log(applyColor.carMaterial);
+        //applyColor.carMaterial.color = Color.white;
+
+        //kan nog de carRotation script hierin samenvoegen
+    }
+
+    private void ResetCarUI()
+    {
+        ccb.isConfOpen = false;
+        carUI.SetActive(false);
     }
 }
